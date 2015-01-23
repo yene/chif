f63d477c2f21888e6116678d2d28ef56553c7b1d
Yannick Weiss <yannick.weiss@goldbachinteractive.ch>
2015-01-23 13:40:44 +0100

    Initial Commit

---
+++ chif/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chif
+//
+//  Created by Yannick Weiss on 23.01.15.
+//  Copyright (c) 2015 GBI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chif/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chif
+//
+//  Created by Yannick Weiss on 23.01.15.
+//  Copyright (c) 2015 GBI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
