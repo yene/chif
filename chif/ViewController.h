259d2a26cfef3385b824ff85eb48e877c3c312d5
Yannick Weiss <yannick.weiss@goldbachinteractive.ch>
2015-01-23 14:07:27 +0100

    displaying a gif

--- chif/ViewController.h
+++ chif/ViewController.h
@@ -9,6 +9,17 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+- (IBAction)previous:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *previous;
+
+- (IBAction)next:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *next;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+- (IBAction)save:(id)sender;
+
+- (IBAction)toggleNSFW:(id)sender;
 
 
 @end
--- chif/ViewController.m
+++ chif/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "UIImage+animatedGIF.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    BOOL NSFW;
+}
+
 
 @end
 
@@ -17,6 +21,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImage *gif = [UIImage animatedImageWithAnimatedGIFURL:[NSURL URLWithString:@"http://i.imgur.com/07tFt.gif"]];
+    self.imageView.image = gif;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +31,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)next:(id)sender {
+}
+- (IBAction)previous:(id)sender {
+}
+- (IBAction)save:(id)sender {
+}
+
+- (IBAction)toggleNSFW:(id)sender {
+    NSFW = !NSFW;
+    
+    if (NSFW) {
+        [sender setTitle:@"NSFW on" forState:UIControlStateNormal];
+    } else {
+        [sender setTitle:@"NSFW off" forState:UIControlStateNormal];
+    }
+}
 @end
